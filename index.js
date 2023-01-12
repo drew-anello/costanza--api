7d0bbf77a77f853e74e772130736a60d4085dffb
Drew Anello <drew.anello@gmail.com>
2023-01-12 13:41:52 -0500

    wip

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('whadup')
+  res.status(200).send({
+    outfit: 'Gore tex',
+    img: 'https://pbs.twimg.com/media/C24u9TsWEAIc4O9.jpg'
+  })
 })
 
 app.listen(3000, () => {

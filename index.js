4d53cd9768f7c1675b5fc0b64d401e4763460061
Drew Anello <drew.anello@gmail.com>
2023-01-11 19:52:53 -0500

    server is working

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const app = require('express')
-const PORT = 8080
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('whadup')
+})
+
+app.listen(3000, () => {
+  console.log('were live mang')
+})

c15c22d4691d5019c3179329b04aadfb7cae04ce
Drew Anello <drew.anello@gmail.com>
2023-01-13 14:01:55 -0500

    adds mongoose connection

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+mongoose.connect('mongodb://localhost/costanzachic')
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('connected to db'))
 app.get('/', (req, res) => {
   res.status(200).send({
     outfit: 'Gore tex',

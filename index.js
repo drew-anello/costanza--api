5384d42151a1ae8aa37eff162d80be8331237d3c
Drew Anello <drew.anello@gmail.com>
2023-01-25 23:17:04 -0500

    adds routes folder and episode.js file

--- index.js
+++ index.js
@@ -7,13 +7,9 @@ mongoose.connect(process.env.DB_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('connected to db'))
-app.get('/', (req, res) => {
-  res.status(200).send({
-    outfit: 'Gore tex',
-    img: 'https://pbs.twimg.com/media/C24u9TsWEAIc4O9.jpg'
-  })
-})
-
+app.use(express.json())
+const episodesRouter = require('./routes/episodes')
+app.use('/', episodesRouter)
 app.listen(3000, () => {
   console.log('I was in the pool! I WAS IN THE POOL!')
 })

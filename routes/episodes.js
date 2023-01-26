54f6f00d6d3891716e7540110d62daa0b64af414
Drew Anello <drew.anello@gmail.com>
2023-01-26 00:27:47 -0500

    first set of data displaying in api

--- routes/episodes.js
+++ routes/episodes.js
@@ -1,13 +1,34 @@
 const express = require('express')
+const quote = require('../models/quote')
 const router = express.Router()
 const Quote = require('../models/quote')
 
 // getting all episodes
-router.get('/', (req, res) => {
-  res.send('I was in the pool, I was in the Pool!')
+router.get('/', async (req, res) => {
+  try {
+    const quote = await Quote.find()
+    res.json(quote)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 })
 // getting one episode
 router.get('/:id', (req, res) => {
   res.send(req.params.id)
 })
+router.post('/', async (req, res) => {
+  const quote = new Quote({
+    quote: req.body.quote,
+    name: req.body.name,
+    season: req.body.season,
+    episode: req.body.episode
+  })
+  try {
+    const newQuote = await quote.save()
+    res.status(201).json(newQuote)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
+
 module.exports = router
